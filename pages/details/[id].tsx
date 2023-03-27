e319b3c27b90041f52dda8a211d151390abb64c7
Ade Indriawan <aindriawan1@gmail.com>
2023-03-27 13:12:50 +0700

    Renamed URL to follow env vars

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -45,9 +45,8 @@ const sections = [
 const theme = createTheme();
 
 export async function getData(){
-  const res = await fetch('http://academy.beta/api/test/response');
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/courses/data`);
   const data = await res.json();
-  // console.log(data);
   return data;
 }
 
